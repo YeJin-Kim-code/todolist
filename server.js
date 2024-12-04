9425b43f3668fce0e2620f39195acbdd4d2ea7ad
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2024-12-04 17:06:30 +0900

    feat server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.listen(8080, function(){
+    //8080이라는 포트에 서버 띄어주세요
+    console.log('listening on 8080');
+});//컴터에 서버열기 
+//서버를 띄우기 위한 기본 셋팅
