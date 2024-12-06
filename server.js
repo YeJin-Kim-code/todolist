c60eebf84665d24a89405d517b7e7e5b67cb8381
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2024-12-06 13:46:46 +0900

    feat server request

--- server.js
+++ server.js
@@ -3,5 +3,14 @@ const app = express();
 app.listen(8080, function(){
     //8080이라는 포트에 서버 띄어주세요
     console.log('listening on 8080');
+    //서버를 띄우기 위한 기본 셋팅
+
 });//컴터에 서버열기 
-//서버를 띄우기 위한 기본 셋팅
+
+//누군가가 /pet으로 방문을 하면..
+//pet관련된 안내문을 띄워주자
+app.get('/pet', function(요청, 응답){
+        응답.send('펫용품 쇼핑할 수 있는 페이지입니다.');
+})
+
+//ctrl+c 서버꺼주기
